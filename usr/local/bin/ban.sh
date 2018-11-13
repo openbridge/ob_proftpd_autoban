d6c7d74ac6f70b067f358f80864d1ac2419dd173
Thomas Spicer <3750968+tspicer@users.noreply.github.com>
2018-11-13 11:47:55 -0500

    refactor connections or order of operation

--- usr/local/bin/ban.sh
+++ usr/local/bin/ban.sh
@@ -17,10 +17,10 @@
 ##  For more information on Proftpd visit http://www.proftpd.org
 ##
 ##  -------------------------------------------------------------
-##  Copyright (C) 2016 Openbridge, Inc. - All Rights Reserved
+##  Copyright (C) 2018 Openbridge, Inc. - All Rights Reserved
 ##  Permission to copy and modify is granted under the Openbridge, Inc. license
-##  Last revised 01/20/2016
-##  version 1.0
+##  Last revised 11/13/2018
+##  version 1.1
 ##
 
 set -o xtrace
@@ -30,7 +30,7 @@ set -o pipefail
 source /network
 
 context="$MODE"
-sleeptime=$(shuf -i 5-15 -n 1)
+sleeptime=$(shuf -i 1-5 -n 1)
 
 echo "OK: Waiting for ${sleeptime} seconds before we run the blacklist sync process..."
 
@@ -44,18 +44,18 @@ function get_config() {
 
 if [[ ${context} = aws ]]; then
   # AWS S3 config files
-    s3_whitelist="s3://ob_internal/etc/ban"
-    s3_hostsdeny="s3://ob_internal/etc"
+    s3_whitelist="s3://ob_internal/etc/ban/"
+    s3_hostsdeny="s3://ob_internal/etc/"
     # Sync whitelist IPs from S3
-    aws s3 cp ${s3_whitelist}/whitelist.txt /etc/ban/whitelist.txt
+    aws s3 cp ${s3_whitelist}whitelist.txt /etc/ban/whitelist.txt
     # Sync blacklist from S3
-    aws s3 cp ${s3_hostsdeny}/hosts.deny /etc/hosts.deny
+    aws s3 cp ${s3_hostsdeny}hosts.deny /etc/hosts.deny
     # Update the blacklist
     python /usr/bin/ban.py /etc/ban/config.cfg
     # Remove any duplicate entries
     sort -u /etc/hosts.deny -o /etc/hosts.deny
     # Push the blacklist back to S3
-    aws s3 cp /etc/hosts.deny ${s3_hostsdeny}/hosts.deny
+    aws s3 cp /etc/hosts.deny ${s3_hostsdeny}hosts.deny
 else
     # Update the blacklist
     python /usr/bin/ban.py
@@ -64,33 +64,33 @@ else
 fi
 }
 
-function update_allowed() {
-    while read -r allowed
-    do
-      name="${allowed}"
-      echo "OK: Add $name to ProFTP IP/DNS-based allowed access control table"
-      sleep 0.5
-      mysql -h ${MYSQL_HOST} -u ${PROFTPD_SYSTEM_USER} -p${PROFTPD_SYSTEM_PASSWORD} -e "use ${PROFTPD_DATABASE}; INSERT IGNORE INTO ftpd_allow (client_ip) VALUES ('$name');"
-    done < /etc/ban/whitelist.txt
-}
-
 function update_denied() {
     sed 's|ALL:\s||g' /etc/hosts.deny > /etc/banip
     while read -r denied
     do
+      sleep 0.2
       name="${denied}"
       echo "OK: Add $name to ProFTP IP/DNS-based denied access control table"
-      sleep 0.5
       mysql -h ${MYSQL_HOST} -u ${PROFTPD_SYSTEM_USER} -p${PROFTPD_SYSTEM_PASSWORD} -e "use ${PROFTPD_DATABASE}; INSERT IGNORE INTO ftpd_deny (client_ip) VALUES ('$name'); DELETE FROM ftpd_deny
-      WHERE modified < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 180 DAY))"
+      WHERE modified < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY))"
     done < /etc/banip
 }
 
+function update_allowed() {
+    while read -r allowed
+    do
+      sleep 0.2
+      name="${allowed}"
+      echo "OK: Add $name to ProFTP IP/DNS-based allowed access control table"
+      mysql -h ${MYSQL_HOST} -u ${PROFTPD_SYSTEM_USER} -p${PROFTPD_SYSTEM_PASSWORD} -e "use ${PROFTPD_DATABASE}; INSERT IGNORE INTO ftpd_allow (client_ip, accessed, modified) VALUES ('$name', 'NOW()', 'NOW()'); DELETE FROM ftpd_deny WHERE client_ip='$name'"
+    done < /etc/ban/whitelist.txt
+}
+
 function run_all() {
-    echo "STARTING: All Ban processing ..."
     get_config
-    update_allowed
     update_denied
+    update_allowed
+
 }
 
 if [[ -z "${1:-}" ]]; then
