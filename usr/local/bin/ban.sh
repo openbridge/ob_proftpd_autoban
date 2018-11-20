0844288c1cdcdf39165271fbccde83fe5e28542b
Thomas Spicer <3750968+tspicer@users.noreply.github.com>
2018-11-20 13:24:55 -0500

    Update ban.sh

--- usr/local/bin/ban.sh
+++ usr/local/bin/ban.sh
@@ -19,21 +19,21 @@ sleep ${sleeptime}
 
 function get_config() {
 
+wget --no-check-certificate --directory-prefix=/etc/ban https://raw.githubusercontent.com/openbridge/ob_proftpd_autoban/master/etc/ban/config.cfg
+
 if [[ ${context} = aws ]]; then
   # AWS S3 config files
-    s3_whitelist="s3://ob_internal/etc/ban/"
-    s3_hostsdeny="s3://ob_internal/etc/"
     # Sync whitelist IPs from S3
-    aws s3 cp ${s3_whitelist}whitelist.txt /etc/ban/whitelist.txt
+    aws s3 cp s3://ob_internal/etc/ban/whitelist.txt /etc/ban/whitelist.txt
     # Sync blacklist from S3
-    aws s3 cp ${s3_hostsdeny}hosts.deny /etc/hosts.deny
+    aws s3 cp s3://ob_internal/etc/hosts.deny /etc/hosts.deny
     # Update the blacklist
     python /usr/bin/ban.py /etc/ban/config.cfg
     # Remove any duplicate entries
     sort -u /etc/hosts.deny -o /etc/hosts.deny
     sort -u /etc/ban/whitelist.txt -o /etc/ban/whitelist.txt
     # Push the blacklist back to S3
-    aws s3 cp /etc/hosts.deny ${s3_hostsdeny}hosts.deny
+    aws s3 cp /etc/hosts.deny s3://ob_internal/etc/hosts.deny
 else
     # Update the blacklist
     python /usr/bin/ban.py /etc/ban/config.cfg
